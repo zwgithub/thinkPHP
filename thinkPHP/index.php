f40e07858dab238761965726b4adc4da63560330
zwgithub <caozhenwei8888@gmail.com>
2016-03-13 16:44:50 +0800

    初始化框架

---
+++ thinkPHP/index.php
@@ -0,0 +1,12 @@
+<?php
+//包含入口文件
+include ('./ThinkPHP/ThinkPHP.php');
+
+//定义框架路径
+define ( 'THINK_PATH', './ThinkPHP/' );
+
+//定义项目名称
+define ( 'APP_NAME', 'thinkPHP' );
+
+//定义项目文件
+define ( 'APP_PATH', './' );
